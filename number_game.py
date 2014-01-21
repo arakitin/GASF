5334c5ec1a799e98f8a6dd4e252bcb5f84088264
Alexander Rakitin <rakitin75@gmail.com>
2014-01-20 20:49:38 -0800

    First lesson

---
+++ number_game.py
@@ -0,0 +1,19 @@
+
+x = 0
+max_tries = 10
+count = 0
+
+while True:
+    x_new = int(raw_input('New number:'))
+    if x_new > x:
+        print '>'
+    elif x_new < x:
+        print '<'
+    else:
+        print 'same'
+        break
+    x = x_new
+    count += 1
+    if count > max_tries:
+        print 'too many'
+        break
